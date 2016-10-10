fec8be7c64cbb252e549ef363e78a284970e3fe6
FNickolai <giraffe.in.ua@gmail.com>
2016-10-10 15:13:39 +0300

    Completed setting up Firebase integration

---
+++ Pods/Target Support Files/Pods-giraffe-social-firebase/Pods-giraffe-social-firebase-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_giraffe_social_firebaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_giraffe_social_firebaseVersionString[];
+
